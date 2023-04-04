b2c8015dea2903a211f38f8afad920dfef1d7f24
JOAO ALFREDO PAGENO <joao_pageno@estudante.sc.senai.br>
2023-04-04 20:05:32 -0300

    first commit endpoint7

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('./controllers/endpoint-7');
+
+router.get('/menorPlacar', controller.consultarmenorplacar);
+router.put('/atualizarNomeTime', controller.atualizarnometime);
 
 module.exports = router;
