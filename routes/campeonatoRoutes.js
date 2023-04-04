4ca1c4cfc10725675c17ec4f76b4a5f6648d0fcd
EmelySanto <emelysantina00@gmail.com>
2023-04-04 20:56:06 -0300

    feat:ConsulTeams-insertTeamsPoints

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const consultTeams = require("../controllers/consultTeams");
+const insertTeams = require("../controllers/insertTeamspoints");
+
+router.get('/Times', consultTeams.getTimes)
+router.post('/Times/insere', insertTeams.insertTeam)
+
+
 module.exports = router;
