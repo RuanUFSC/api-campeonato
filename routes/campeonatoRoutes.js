51e6f8d76ab1b02bb67da9cd0f80be8706df26e3
lucasfurlani96 <lucas_furlani@estudante.sc.senai.br>
2023-04-04 22:09:51 -0300

    Consultar equipes por ordem de pontuação crescente

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -5,5 +5,6 @@ const controller = require('./controllers/endpoint-7');
 router.get('/menorPlacar', controller.consultarmenorplacar);
 router.put('/atualizarNomeTime', controller.atualizarnometime);
 router.delete('/deletar-partida/:id', controller.removerPartida);
+router.get('/consultaAsc', controller.consultaAsc);
 
 module.exports = router;
