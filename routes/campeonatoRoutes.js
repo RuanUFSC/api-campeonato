a94f4ebb7df8a0e6571418900f225a74805b0c40
Waldyr <waldyr.programing@gmail.com>
2023-04-04 21:19:05 -0300

    feat: add 1.2 GET and 2.2 POST methods for registry table

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controller/campeonatoController')
+
+router.get('/consultar-partidas',controller.getConsultarPartidas);
+router.post('/inserir-registro', controller.postInserirRegistro);
+router.delete('/deletar-registro/:id', controller.deleteRegistro);
 
 module.exports = router;
