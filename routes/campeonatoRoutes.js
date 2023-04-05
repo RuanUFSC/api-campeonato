d3a23990fa90a0fce4f7f2a7bfc388112ef6718d
Emely <89606365+EmelySanto@users.noreply.github.com>
2023-04-04 21:19:27 -0300

    Revert "Develop"

--- routes/campeonatoRoutes.js
+++ routes/campeonatoRoutes.js
@@ -1,16 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('./controllers/endpoint-7');
-
-router.get('/menorPlacar', controller.consultarmenorplacar);
-router.put('/atualizarNomeTime', controller.atualizarnometime);
-router.delete('/deletar-partida/:id', controller.removerPartida);
-
-const consultTeams = require("../controllers/consultTeams");
-const insertTeams = require("../controllers/insertTeamspoints");
-
-router.get('/Times', consultTeams.getTimes)
-router.post('/Times/insere', insertTeams.insertTeam)
-
 
 module.exports = router;
